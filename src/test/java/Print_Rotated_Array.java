161a3166dc71c355145342eb519c480752fe32ac
indeswami <inder.swami@gmail.com>
2024-05-24 10:37:40 +0530

    na

--- src/test/java/Print_Rotated_Array.java
+++ src/test/java/Print_Rotated_Array.java
@@ -1,22 +1,41 @@
 public class Print_Rotated_Array {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6,7};
-        int temp=0;
-        int start=0;
-        int end=arr.length-1;
-        for (int i=0;i< arr.length-1;i++){
-            System.out.println(arr[i]);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int rotation = 3;
 
-        }
+        System.out.println("Original Array:");
+        printArray(arr);
 
+        rotateArray(arr, rotation);
 
-
-
-
-
-
+        System.out.println("\nArray after rotation:");
+        printArray(arr);
     }
 
+    // Function to rotate the array by given positions
+    public static void rotateArray(int[] arr, int rotation) {
+        int n = arr.length;
+        reverseArray(arr, 0, n - 1);
+        reverseArray(arr, 0, rotation - 1);
+        reverseArray(arr, rotation, n - 1);
+    }
 
+    // Function to reverse the array or a portion of it
+    public static void reverseArray(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
 
+    // Function to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
