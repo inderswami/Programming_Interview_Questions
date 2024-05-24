fef6b8af87873c26296b1d6ddf3099f35e411ddb
indeswami <inder.swami@gmail.com>
2024-05-24 10:40:02 +0530

    na

--- src/test/java/Print_Sum_Equal_Target_Display_Index.java
+++ src/test/java/Print_Sum_Equal_Target_Display_Index.java
@@ -1,27 +1,16 @@
-//public class Sum_Equal_Target_Display_Index {
-//    int[] arr={1,2,4,5,6};
-//    int n= arr.length;
-//    int target=6;
-//for(int i=0; i<n; i++){
-//        for (int j=i+1;j<n;j++){
-//
-//            if (arr[i]+arr[j]==target){
-//                System.out.println(i);
-//                System.out.println(j);
-//            }
-//        }
-//
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//}
-//
-//
-//
-//
+public class Print_Sum_Equal_Target_Display_Index {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6};
+        int n = arr.length;
+        int target = 6;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println("Index 1: " + i);
+                    System.out.println("Index 2: " + j);
+                }
+            }
+        }
+    }
+}
